9c4dc13bbe01d64cd6b367d0fc10bab9230c4bb3
abahuje <afabiorah@gmail.com>
2023-04-21 23:19:43 +0200

    My own resume

---
+++ index.js
@@ -0,0 +1,30 @@
+// function sortedItem(){
+//     let message="hello world";
+//     let str=message.split("")
+//     let newOne=str.sort()
+//     let helloworld=newOne.join("")
+//     console.log(str)
+// }
+// sortedItem()
+
+//  let z=(22,33,44,55,66)
+//   for(a=0;a<30;a++)
+//  console.log()
+// var place=document.getElementById('nickname')
+// var p=document.createElement('p');
+// var text=document.createTextNode('Benie')
+// p.appendChild(text);
+// place.parentNode.insertBefore(place,p);
+document.body.style.background="grey"
+document.querySelector("body").style.fontFamily="monospace"
+document.querySelector("body").style.fontSize="24px"
+ document.querySelector("#nickname").innerText="boy"
+ document.querySelector("#favorites").innerHTML="ball"
+ document.querySelector("#hometown").innerHTML="bale"
+
+ var list = document.querySelector("li")
+ list.setAttribute("class","listitem") 
+//  for(var i=0; i<list.length; i++){
+    // list[i].setAttribute("class","listitem") }
+// document.querySelectorAll(".listitem").style.color = "red"
+console.log(list)
